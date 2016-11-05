18e6850c88a160cc481e35f5223ed01b4ca097fe
Lucia Salvarrey <lsalvarrey@altimetrik.com>
2016-11-05 18:02:42 -0300

    Updated Currency and Important controllers for wrong search reference usage

--- backend/controllers/CurrencyController.php
+++ backend/controllers/CurrencyController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 
 use Yii;
 use backend\models\Currency;
-use backend\controllers\CurrencySearch;
+use backend\models\CurrencySearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
