fe55ec215ecf98589c8e66d9729a9f9cba7a526a
kavesa.laptop <kavesa@gmail.com>
2016-11-15 11:11:33 -0300

    removing aws key and secret

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php
 return [
 	's3' => [
-		'key' => 'AKIAI3ZMXRFYE6X2TTFQ',
-		'secret' => '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',
+		'key' => 'removed',
+		'secret' => 'removed',
 		'bucket' => 'php12'
 	]
 ];
