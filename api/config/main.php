03dbffcef6a0dd2a9fb774459ccfdec87227cbae
kavesa.laptop <kavesa@gmail.com>
2016-11-15 11:07:45 -0300

    update aws key and secret

--- api/config/main.php
+++ api/config/main.php
@@ -16,6 +16,23 @@ return [
         'v1' => [
             'basePath' => '@app/modules/v1',
             'class' => 'api\modules\v1\Module'
+        ],
+        'oauth2' => [
+            'class' => 'filsh\yii2\oauth2server\Module',            
+            'tokenParamName' => 'accessToken',
+            'tokenAccessLifetime' => 3600 * 24,
+            'storageMap' => [
+                'user_credentials' => 'app\models\User',
+            ],
+            'grantTypes' => [
+                'user_credentials' => [
+                    'class' => 'OAuth2\GrantType\UserCredentials',
+                ],
+                'refresh_token' => [
+                    'class' => 'OAuth2\GrantType\RefreshToken',
+                    'always_issue_new_refresh_token' => true
+                ]
+            ]
         ]
     ],
     'components' => [        
