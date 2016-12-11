d1cfa58516340800ab0458b9ab7714262106cd90
Lucia Salvarrey <lsalvarrey@altimetrik.com>
2016-12-11 20:49:59 -0300

    Updated admin lte

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -5,61 +5,33 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="../files/default_profile.png" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p><?php echo Yii::$app->user->identity->username; ?></p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
 
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
-        <!-- /.search form -->
+        
 
-        <?= dmstr\widgets\Menu::widget(
+        <?= 
+        dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Same tools',
-                        'icon' => 'fa fa-share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'fa fa-circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'fa fa-circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
+                    ['label' => 'Menu', 'options' => ['class' => 'header']],
+
+                    
+                    ['label' => 'Tipos de Propiedades', 'icon' => 'fa fa-square', 'url' => ['/property-type']],
+                    ['label' => 'Departamento', 'icon' => 'fa fa-leaf', 'url' => ['/state']],
+                    
+                    ['label' => 'Barrio', 'icon' => 'fa fa-bank', 'url' => ['/neighbourhood']],
+                    ['label' => 'Propiedades', 'icon' => 'fa fa-home', 'url' => ['/property']],
+                    ['label' => 'Clientes', 'icon' => 'fa fa-user', 'url' => ['/client']],
+                    ['label' => 'Monedas', 'icon' => 'fa fa-circle', 'url' => ['/currency']],
+                    ['label' => 'Tipos de operación', 'icon' => 'fa fa-flag', 'url' => ['/operation-type']],
                 ],
             ]
         ) ?>
